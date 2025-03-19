616536662a8f76b6ba66debd990f5b5d8b2b7e6a
test <mohdhisyamudin.b@tcs.com>
2025-03-20 00:25:58 +0800

    use neon db

--- src/main/java/com/FaiqsProject/BillSplitter/model/Users.java
+++ src/main/java/com/FaiqsProject/BillSplitter/model/Users.java
@@ -1,12 +1,16 @@
 package com.FaiqsProject.BillSplitter.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Users {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private int id;
     private String username;
     private String password;
